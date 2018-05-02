912007abefe7f27fde1c008582d0cd407eee7fd7
Matt DeVoe <matthew.devoe@gmail.com>
2018-05-02 10:37:43 -0600

    newquote component added

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
     <div class="container">
-    
+      <appNewQuote></appNewQuote>
+      <appQuoteGrid :quotes="quotes"></appQuoteGrid>
     </div>
 </template>
 
 <script>
-    export default {
-      data: function() {
-        return {
-          maxQuotes: 10,
-          quotes: [
-            'Just to see something'
-          ]
-        }
+  import QuoteGrid from './components/QuoteGrid.vue'
+  import NewQuote from './components/NewQuote.vue'
+  
+  export default {
+    components: {
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote
+    },
+    data: function() {
+      return {
+        maxQuotes: 10,
+        quotes: [
+          'Just to see something'
+        ]
       }
     }
+  }
 </script>
 
 <style>
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="row">
-    
+    <appQuote v-for="quote in quotes">{{ quote }}</appQuote>
   </div>
 </template>
 
 <script>
+  import Quote from './Quote.vue'
+
   export default {
-    props: ['quotes']
+    props: ['quotes'],
+    components: {
+      appQuote: Quote
+    }
   }
 </script>
 
